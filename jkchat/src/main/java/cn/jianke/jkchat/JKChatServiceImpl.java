fec3564937655b6d382ad539240055a087d713a8
雷兵 <leibing@jianke.com>
2017-02-21 16:00:08 +0800

    Update_improve

--- jkchat/src/main/java/cn/jianke/jkchat/JKChatServiceImpl.java
+++ jkchat/src/main/java/cn/jianke/jkchat/JKChatServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import cn.jianke.jkchat.common.RequestUrlUtils;
 import cn.jianke.jkchat.common.StringUtils;
 import cn.jianke.jkchat.data.Shareperferences.PatientShareperferences;
+import cn.jianke.jkchat.domain.JkChatConversation;
 import cn.jianke.jkchat.domain.JkChatMessage;
 import cn.jianke.jkchat.domain.JkChatSession;
 import cn.jianke.jkchat.httprequest.JkChatRequest;
@@ -42,9 +43,12 @@ public class JKChatServiceImpl implements JkChatService{
     private final static String CONNECTED = "connected";
     // 连接状态----断开连接
     private final static String DISCONECTED = "disconected";
+    // 常见对医生说谢谢的话
+    private final static String[] thanks = {"谢谢医生", "谢了",
+            "谢谢您", "非常感谢", "下次还找你"};
     // 连接状态
     private String connectStatus = INIT;
-    // Context weakRef
+    // Context weak Ref
     private WeakReference<Context> mContextWeakRef;
     // 健客聊天服务实现单例
     private static JKChatServiceImpl instance;
@@ -64,6 +68,21 @@ public class JKChatServiceImpl implements JkChatService{
     private boolean isForbidAsk = false;
     // 标识用户会话状态
     private int askingFlag;
+    // 会话是否结束
+    private boolean isConversationFinished = false;
+    // 医生客户端关闭监听
+    private OnClientClosedListener mOnClientClosedListener;
+    // 健客聊天消息列表
+    public static List<JkChatMessage> tmpMessages = new ArrayList<JkChatMessage>();
+    // 用户id
+    private String customSessionID = "Unknow_CustomSessionID";
+    // 客服id
+    private String staffSessionID = "Unknow_StaffSesionID";
+    // 健客聊天会话
+    private JkChatConversation jkCurrentConversation;
+    // 是否完成首次打开fragment时，对服务器的请求 保证请求完服务器信息才允许发送信息
+    // 因为考虑到网速慢，导致发送消息的时候，第一次请求还没响应，破坏了期望的连接状态
+    private boolean isFinishedFirstRequest = false;
     // 健客聊天api监听
     private JkChatApiListener mJkChatApiListener = new JkChatApiListener() {
         @Override
@@ -96,19 +115,24 @@ public class JKChatServiceImpl implements JkChatService{
 
         @Override
         public void onCheckUserAskingNow(int askingFlag) {
-
+            // 设置标识用户会话状态
+            setAskingFlag(askingFlag);
         }
 
         @Override
         public void onCheckUserAskingNowError(Exception e) {
-
+            // 网络失败回调（网络连接失败）
+            if (mJkChatServiceListener != null)
+                mJkChatServiceListener.onNetworkUnavaiable(NETWORK_CONNECTION_FAILED);
         }
     };
+
     // 健客聊天连接监听
     private JkChatConnectionListener mJkChatConnectionListener = new JkChatConnectionListener() {
         @Override
         public void onOpen() {
-
+            // 设置会话未结束
+            isConversationFinished = false;
         }
 
         @Override
@@ -146,61 +170,159 @@ public class JKChatServiceImpl implements JkChatService{
                     }
                 });
             }
+
+            try {
+                // 设置连接状态为断开
+                connectStatus = DISCONECTED;
+                // 设置会话已结束
+                isConversationFinished = true;
+                // 设置已完成第一次请求
+                isFinishedFirstRequest = true;
+            } catch (Exception e) {
+            } finally {
+                if (code != WebSocket.ConnectionHandler.CLOSE_NORMAL
+                        && code != WebSocket.ConnectionHandler.CLOSE_CONNECTION_LOST) {
+                    // 若连接状态非连接中或已连上则回调服务器连接失败
+                    if (connectStatus != CONNECTING
+                            || connectStatus != CONNECTED) {
+                        if (mJkChatServiceListener != null)
+                            mJkChatServiceListener.onNetworkUnavaiable(SERVER_CONNECTION_FAILED);
+                    }
+                }
+            }
         }
 
         @Override
         public void onStuffNumChanged(int number) {
-
+            // 再次判断医生在线数目
+            // 设置医生在线数目
+            setDoctorNum(number);
+            // 设置聊天连接状态为正在连接中
+            connectStatus = CONNECTING;
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            if (number < 1){
+                // 发送系统消息
+                sendSystemText(mContextWeakRef.get().getResources().getString(
+                        R.string.chat_few_keyword_doctor_offline_tips));
+            }
         }
 
         @Override
         public void onSessionChanged(JkChatSession session) {
-
+            // 设置聊天连接状态为已经连接上
+            connectStatus = CONNECTED;
+            // 若session为空则返回
+            if (session == null)
+                return;
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            // 有医生接受通话时发送的系统消息
+            sendSystemText(mContextWeakRef.get().getResources().getString(
+                    R.string.chat_doctor_answering_pre)
+                    + session.getStaffName()
+                    + mContextWeakRef.get().getResources().getString(
+                    R.string.chat_doctor_answering));
+            // 客服名称、接收消息回调
+            if (mJkChatServiceListener != null){
+                // 客服名称回调
+                mJkChatServiceListener.onStuffChanged(session.getStaffName());
+                // 组建健客聊天消息
+                JkChatMessage chatMessage = new JkChatMessage();
+                chatMessage.setMsg("");
+                chatMessage.setTid(session.getTid());
+                chatMessage.setMsgType("");
+                // 接收消息回调
+                mJkChatServiceListener.onReceivedMessage(chatMessage);
+            }
         }
 
         @Override
         public void onReceiveMessage(JkChatMessage message) {
-
+            // 回调医生发来的消息
+            if (mOnClientClosedListener != null){
+                mOnClientClosedListener.onReceiveMessage(message);
+            }
+            if (mJkChatServiceListener != null)
+                mJkChatServiceListener.onReceivedMessage(message);
         }
 
         @Override
         public void onClientClosed() {
-
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            // 发送系统消息
+            sendSystemText(mContextWeakRef.get().getResources().getString(
+                    R.string.chat_ending));
+            // 医生客户端关闭时回调或是客户主动关闭在线咨询时回调
+            if (mOnClientClosedListener != null)
+                mOnClientClosedListener.OnClientClosed();
         }
 
         @Override
         public void onStuffOffline() {
-
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            // 发送系统消息
+            sendSystemText(mContextWeakRef.get().getResources().getString(
+                    R.string.chat_doctor_lost_conn));
         }
 
         @Override
         public void onChangetoWait() {
-
+            // 医生把提问者放到等待应答列表，则上一次的tid作废，
+            // 到了再次有医生应答时，即回调onSessionChanged时，会将之前tid为空的消息记录当作是本次会话的记录
+            // 转换多个医生也是如此
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            // 发送系统消息
+            sendSystemText(mContextWeakRef.get().getResources().getString(
+                    R.string.chat_doctor_change_Wait));
         }
 
         @Override
         public void onChatClosed() {
-
+            // 设置连接状态为正在连接中
+            connectStatus = CONNECTING;
         }
 
         @Override
         public void onChatStillAlive() {
-
+            // 设置连接状态为已经连接上
+            connectStatus = CONNECTED;
         }
 
         @Override
         public void onGetServerMessageDone() {
-
+            // 设置已完成第一次请求
+            isFinishedFirstRequest = true;
         }
 
         @Override
         public void onFilterAskQuestion() {
-
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            // 发送系统消息
+            sendSystemText(mContextWeakRef.get().getResources().getString(
+                    R.string.chat_filter_word_tips));
         }
 
         @Override
         public void onForbidAskQuestion() {
-
+            // 设置禁止聊天
+            isForbidAsk = true;
+            // 若页面实例引用为空则返回
+            if (mContextWeakRef == null || mContextWeakRef.get() == null)
+                return;
+            // 发送系统消息
+            sendSystemText(mContextWeakRef.get().getResources().getString(
+                    R.string.chat_limit_ip));
         }
     };
 
@@ -331,12 +453,41 @@ public class JKChatServiceImpl implements JkChatService{
 
     @Override
     public int sendMessage(JkChatMessage msg) {
-        return 0;
+        // 图片消息不用过滤，直接发送
+        if (!msg.getMsgType().equalsIgnoreCase(JkChatMessage.TYPE_IMG)) {
+            int result = filterMessage(msg);
+            // 若不可发送文字则返回
+            if (result != SENDSTATUS_CAN_SEND) {
+                return result;
+            }
+        }
+        // 根据连接状态处理消息发送
+        switch (connectStatus){
+            case INIT:
+            case DISCONECTED:
+                // 连接并发送消息
+                connectAndSendMessage(msg);
+                return SENDSTATUS_FILTERED;
+            case CONNECTING:
+                // 医生未应答时，消息加到消息队列，
+                // 当应答后回调了onOpen时，将遍历消息队列，发送给医生
+                addMsgAndFilterRepeatMsg(msg);
+//                sendAndSaveMsgByLoop();
+                return SENDSTATUS_FILTERED;
+            case CONNECTED:
+                sendAndSaveMessage(msg);
+                break;
+            default:
+                break;
+        }
+
+        return SENDSTATUS_CAN_SEND;
     }
 
     @Override
     public void setListener(JkChatServiceListener listener) {
-
+        // 设置健客聊天服务监听
+        this.mJkChatServiceListener = listener;
     }
 
     @Override
@@ -347,12 +498,12 @@ public class JKChatServiceImpl implements JkChatService{
 
     @Override
     public boolean isFinishedFirstRequest() {
-        return false;
+        return isFinishedFirstRequest;
     }
 
     @Override
     public boolean isConversationFinished() {
-        return false;
+        return isConversationFinished;
     }
 
     @Override
@@ -377,7 +528,10 @@ public class JKChatServiceImpl implements JkChatService{
 
     @Override
     public boolean getConnectStatus() {
-        return false;
+        if (connectStatus == CONNECTED)
+            return true;
+        else
+            return false;
     }
 
     /**
@@ -435,5 +589,192 @@ public class JKChatServiceImpl implements JkChatService{
             if (mJkChatServiceListener != null)
                 mJkChatServiceListener.onReceivedMessage(msg);
         }
+    }
+
+    /**
+     * 过滤消息
+     * @author leibing
+     * @createTime 2017/2/21
+     * @lastModify 2017/2/21
+     * @param  message 健客聊天消息
+     * @return
+     */
+    private int filterMessage(JkChatMessage message) {
+        // 默认可以发送文字
+        int result = SENDSTATUS_CAN_SEND;
+        // 对话结束后，再次发送消息，先执行connectStatus = ConnectStatus.INIT;
+        if (isConversationFinished && (connectStatus != INIT)) {
+            connectStatus = INIT;
+            // 医生结束对话后,原则上是不会进入这里来的
+            // 用户发送语句中包含“谢”字，且字数在10个字以内。常见语句有“谢谢医生”、“谢了”、“谢谢您”、“非常感谢，下次还找你”等等。
+            String msg = message.getMsg();
+            if (msg.length() < 10) {
+                for (int i = 0; i < thanks.length; i++) {
+                    if (msg.contains(thanks[i])) {
+                        if (mJkChatServiceListener != null) {
+                            mJkChatServiceListener.onReceivedMessage(message);
+                            // 若页面实例引用为空则返回
+                            if (mContextWeakRef != null && mContextWeakRef.get() != null) {
+                                sendSystemText(mContextWeakRef.get().getResources().getString(
+                                        R.string.chat_thx2doctor));
+                            }
+                            // 相当于主动结束对话
+                            if (mJkChatConnection != null)
+                                mJkChatConnection.clientPositionClose(message.getTid());
+                            // 清除EditText文字
+                            result = SENDSTATUS_CLEAN_ET;
+                            return result;
+                        }
+                    }
+                }
+            }
+        }
+        // 没有建立对话前，判断字数过滤关键词之类的
+        if (connectStatus == INIT) {
+            String msg = message.getMsg();
+            // 没有建立对话前，判断字数过滤关键词之类的
+            if ((msg.contains("在吗") || msg.contains("有人吗")
+                    || msg.contains("你好") || msg.contains("您好")
+                    || msg.contains("hi") || msg.contains("hello"))
+                    && msg.length() < 10) {
+                // 若禁止聊天则返回禁止提问
+                if (isForbidAsk) {
+                    return SENDSTATUS_FORBID;
+                }
+                // 提醒医生是否在线提示（系统提示）
+                if (mJkChatApi != null)
+                    mJkChatApi.requestGetDoctorNum();
+                // 设置被过滤状态
+                result = SENDSTATUS_FILTERED;
+            } else if (msg.length() < 10) {
+                // 若禁止聊天则返回禁止提问
+                if (isForbidAsk) {
+                    return SENDSTATUS_FORBID;
+                }
+                // 设置被过滤状态
+                result = SENDSTATUS_FILTERED;
+            }
+        }
+        // 若禁止聊天则返回禁止提问
+        if (isForbidAsk) {
+            result = SENDSTATUS_FORBID;
+        }
+        return result;
+    }
+
+    /**
+     * 过滤相同消息
+     * 防止在等待服务器响应时，多次点击发送，造成多条相同的消息
+     * @author leibing
+     * @createTime 2017/2/21
+     * @lastModify 2017/2/21
+     * @param message 健客聊天消息
+     * @return
+     */
+    private void addMsgAndFilterRepeatMsg(JkChatMessage message) {
+        // 图片消息不是通过点击发送按钮发送的，所以不用过滤
+        if (message.getMsgType().equalsIgnoreCase(JkChatMessage.TYPE_IMG)) {
+            tmpMessages.add(message);
+            return;
+        }
+        if (tmpMessages.size() != 0) {
+            for (int i = 0; i < tmpMessages.size(); i++) {
+                if (tmpMessages.get(i).getMsg().equals(message.getMsg())) {
+                    break;
+                }
+                if (i == tmpMessages.size() - 1) {
+                    tmpMessages.add(message);
+                }
+            }
+        } else {
+            tmpMessages.add(message);
+        }
+    }
+
+    /**
+     * 连接并发送消息
+     * @author leibing
+     * @createTime 2017/2/21
+     * @lastModify 2017/2/21
+     * @param message 健客聊天消息
+     * @return
+     */
+    private void connectAndSendMessage(JkChatMessage message) {
+        // 过滤相同消息
+        addMsgAndFilterRepeatMsg(message);
+        // 若连接状态为非正在连接中或已经连接上则需连接健客聊天服务器
+        if (connectStatus != CONNECTING
+                || connectStatus != CONNECTED) {
+            jkConnection();
+        }
+    }
+
+    /**
+     * 发送并保存消息
+     * @author leibing
+     * @createTime 2017/2/21
+     * @lastModify 2017/2/21
+     * @param message 健客聊天消息
+     * @return
+     */
+    private boolean sendAndSaveMessage(JkChatMessage message) {
+        boolean sendMsgSucessed = localSendMessage(message);
+        if (sendMsgSucessed) {
+//            saveMessage(message);
+        }
+        return sendMsgSucessed;
+    }
+
+    /**
+     * 发送聊天信息
+     * @author leibing
+     * @createTime 2017/2/21
+     * @lastModify 2017/2/21
+     * @param message 健客聊天信息
+     * @return
+     */
+    private boolean localSendMessage(JkChatMessage message) {
+        if (mJkChatConnection == null)
+            return false;
+        // 给消息设置用户id
+        message.setCustomSessionID(customSessionID);
+        // 给消息设置客服id
+        message.setStaffSessionID(staffSessionID);
+        // 给消息设置消息id
+        if (jkCurrentConversation != null)
+            message.setTid(jkCurrentConversation.getTid());
+        return mJkChatConnection.sendMessage(message);
+    }
+
+    /**
+     * 设置医生客户端关闭监听
+     * @author leibing
+     * @createTime 2017/2/21
+     * @lastModify 2017/2/21
+     * @param
+     * @return
+     */
+    public void setOnClientClosedListener(OnClientClosedListener mOnClientClosedListener) {
+        this.mOnClientClosedListener = mOnClientClosedListener;
+    }
+    
+    /**
+     * @interfaceName: OnClientClosedListener
+     * @interfaceDescription: 医生客户端关闭的时候调用
+     * @author: leibing
+     * @createTime: 2017/2/21
+     */
+    public interface OnClientClosedListener {
+        /**
+         * 医生客户端关闭的时候回调或是客户主动关闭在线咨询的时候用
+         * @param
+         */
+        void OnClientClosed();
+
+        /**
+         * 医生发送消息过来的时候回调
+         * @param message 医生消息
+         */
+        void onReceiveMessage(JkChatMessage message);
     }
 }
